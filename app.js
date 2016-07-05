f043ecd7bc2516cbf0216784145c999fcdb30025
demigod <drazen.strbad89@gmail.com>
2016-07-05 15:21:31 +0200

    dio login-a,steam guard

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 //Lets require/import the HTTP module
 var dota = require('./dota.js');
+var fs = require('fs');
 var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var Steam = require('steam');
+var crypto = require("crypto")
 var app = express();
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/static'));
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'shhhh, very secret'
+}));
+
+
 
 // use res.render to load up an ejs view file
 
@@ -50,6 +69,58 @@ app.get('/kontakt', function(req, res) {
 //skini
 app.get('/skini', function(req, res) {																																																																																																																																					
     res.render('pages/skini');
+});
+
+app.get('/login', function(req, res) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+    res.render('pages/login');
+});
+
+app.post('/login', function(req, res) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+    var username = req.body.username;
+    var password = req.body.password;
+    console.log(username);
+    console.log(password);
+    var steamClient = new Steam.SteamClient();
+    var steamUser = new Steam.SteamUser(steamClient);
+    var user = null;
+    steamClient.connect();
+    steamClient.on('connected', function() {
+        var SentryData = fs.readFileSync('sentry');
+        steamUser.logOn({
+            account_name: username,
+            password: password,
+            //auth_code: '8VKJ2',
+            sha_sentryfile: SentryData,
+        });
+    });
+    steamClient.on('logOnResponse', function(logonResp) {
+        if (logonResp.eresult == Steam.EResult.OK) {
+            console.log('Logged in!');
+            console.log(logonResp);
+            user = username;
+        }
+        if (user) {
+            // Regenerate session when signing in
+            // to prevent fixation
+            req.session.regenerate(function(){
+                // Store the user's primary key
+                // in the session store to be retrieved,
+                // or in this case the entire user object
+                req.session.user = username;
+                res.redirect('/');
+            });
+        } else {
+            console.log('Neispravni login podaci');
+            res.redirect('/login');
+        }
+    });
+    steamUser.on('updateMachineAuth', function(response, callback){
+        var hashedSentry = crypto.createHash('sha1').update(response.bytes).digest();
+        fs.writeFileSync('sentry', hashedSentry)
+        callback({
+            sha_file: hashedSentry
+        });
+    });
 });
 
 
